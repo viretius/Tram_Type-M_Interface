53834fcf1d2e76505254d72420871e41d631d335
viretius <108616649+viretius@users.noreply.github.com>
2024-08-27 01:05:56 +0200

    Delete typeM_interface directory

--- typeM_interface/lib/typeM_interface/src/typeM_interface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include "config.h"
-#include <trainsim_interface/trainSim_interface.h>
-#include <lokSim3D_interface/lokSim3D_interface.h>
-
-void init_typeM_interface();
-
