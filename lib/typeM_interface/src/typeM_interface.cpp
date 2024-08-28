dd031a568c65ed0027ee6120f10f5682de3e6c2c
viretius <108616649+viretius@users.noreply.github.com>
2024-08-28 18:02:34 +0200

    Update typeM_interface.cpp

--- lib/typeM_interface/src/typeM_interface.cpp
+++ lib/typeM_interface/src/typeM_interface.cpp
@@ -1,34 +1,4 @@
-/*
-https://microchip.my.site.com/s/article/GPA7---GPB7-Cannot-Be-Used-as-Inputs-In-MCP23017
-
-In the datasheet Revision D (June 2022), GPA7 & GPB7 are mentioned
-as outputs only for MCP23017. But, the register bits do allow for 
-the direction of these I/O pins to be changed to inputs. However, 
-the SDA signal can be corrupted during a reading of these bits if 
-the pin voltage changes during the transmission of the bit. It has 
-also been reported that this SDA corruption can cause some bus hosts 
-to malfunction. 
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!In conclusion, GPA7 & GPB7 should only be used as outputs to avoid future issues.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-MCP23017 and PCF8591 i2C Addresses based on wiring 
-
-MCP-Address  A2   A1    A0   PCF-Address
-    0x20    GND  GND   GND   0x48
-    0x21    GND  GND   3V3   0x49
-    0x22    GND  3V3   GND   0x4A
-    0x23    GND  3V3   3V3   0x4B
-    0x24    3V3  GND   GND   0x4C
-    0x25    3V3  GND   3V3   0x4D
-    0x26    3V3  3V3   GND   0x4E
-    0x27    3V3  3V3   3V3   0x4F
-
-*/
-
 #include <typeM_interface.h>
-
-
-
 
 
 void init_typeM_interface() {
@@ -40,7 +10,7 @@ void init_typeM_interface() {
     USB.begin();
     while(!USBSerial);
     find_and_print_partitions();
-
+    
     //====================================================================================
     //actual application initialization
     //====================================================================================
@@ -50,15 +20,11 @@ void init_typeM_interface() {
     uint8_t interface_choice = preferences.getUInt("app_setting", 0);
     preferences.end();
     if(interface_choice == 2) {
-        USBSerial.print(F("\nLokSim3D Setup..."));
+        USBSerial.print(F("LokSim3D Setup...\n"));
         lokSim3D_interface::init();
     } else  {
-        USBSerial.print(F("\nSimMetro Setup..."));
+        USBSerial.print(F("SimMetro Setup...\n"));
         simMetro_interface::init();
     }
-
-
-    
-
-}   
+}
 
