1adc5fee4d097378b06fa4f33dba70698c2b68ae
viretius <108616649+viretius@users.noreply.github.com>
2024-08-31 21:15:29 +0200

    Update config_file_utils.cpp

--- lib/typeM_interface/src/lokSim3D_interface/config_file_utils.cpp
+++ lib/typeM_interface/src/lokSim3D_interface/config_file_utils.cpp
@@ -196,10 +196,34 @@ bool load_config()
         memset(pcf_list[i2c[t] - PCF_I2C_BASE_ADDRESS].address[pin[t]], '\0', 3); // Clear the memory
         
 
-        if (strcmp(address[t], "ct") == 0) {
-          combined_throttle_pin[0] = i2c[t];
-          combined_throttle_pin[1] = pin[t]; 
+        
+        else if (strncmp(address[t], "ct(", 3) == 0) {
+    combined_throttle_pin[0] = i2c[t];
+    combined_throttle_pin[1] = pin[t];
+
+    // Suchen der öffnenden und schließenden Klammer
+    char* start = strchr(address[t], '(');
+    char* end = strchr(address[t], ')');
+
+    if (start != nullptr && end != nullptr && start < end) {
+        *end = '\0'; // Zeichenkette an der schließenden Klammer beenden
+        char* numbers = start + 1; // Zeiger auf die Zahlen nach '('
+
+        // Trennen der Zahlen mit strtok
+        char* first_number = strtok(numbers, ",");
+        char* second_number = strtok(nullptr, ",");
+
+        if (first_number != nullptr && second_number != nullptr) {
+            combined_throttle_pin[3] = static_cast<uint8_t>(atoi(first_number));
+            combined_throttle_pin[4] = static_cast<uint8_t>(atoi(second_number));
+        } else {
+            USBSerial.println("Fehler: Ungültiges Format für ct-Kanalnummern.");
         }
+    } else {
+        USBSerial.println("Fehler: Klammern fehlen oder sind falsch platziert.");
+    }
+}
+
         else {    
             // Copy the key value to the dynamically allocated memory
             strcpy(pcf_list[i2c[t] - PCF_I2C_BASE_ADDRESS].address[pin[t]], address[t]);  
