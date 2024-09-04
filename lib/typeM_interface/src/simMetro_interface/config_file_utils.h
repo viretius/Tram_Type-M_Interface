4572d34652a815fe454d25bc6d416d2b94c9f580
viretius <108616649+viretius@users.noreply.github.com>
2024-09-04 18:55:32 +0200

    Update config_file_utils.h

--- lib/typeM_interface/src/simMetro_interface/config_file_utils.h
+++ lib/typeM_interface/src/simMetro_interface/config_file_utils.h
@@ -1,12 +1,9 @@
 #ifndef TRAINSIM_CONFIG_FILE_UTILS
 #define TRAINSIM_CONFIG_FILE_UTILS
 
+#include <config.h>
+#include <utils.h>
 #include <simMetro_interface/simMetro_interface.h> //mcp_list and pcd_list
-
-#include <FS.h>
-#include <LittleFS.h>
-#include <FFat.h>
-#include <CSV_Parser.h>
 
 namespace simMetro_config{
     bool load_config();
